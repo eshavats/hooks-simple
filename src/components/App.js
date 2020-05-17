0a7b4716207a229102cbe2a978f9c3d206f02f8d
Esha Vats <56391290+eshavats@users.noreply.github.com>
2020-05-17 15:53:38 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+import ResourceList from "./ResourceList";
+import UserList from "./UserList";
+
+const App = () => {
+
+    const [ resource, setResource ] = useState("Posts");
+
+        return (
+            <div>
+            <UserList />
+                <div>
+                    <button onClick={() => setResource("Posts")}>Posts</button>
+                    <button onClick={() => setResource("Todos")}>Todos</button>
+                </div>
+                <ResourceList resource={resource} />
+            </div>
+        );  
+};
+
+export default App;
